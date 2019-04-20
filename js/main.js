5f4c80e0b9448358042f435d151decb6ec5f506b
AbdullahAmeen <46934578+AbdullahAmeen@users.noreply.github.com>
2019-04-20 18:48:05 -0500

    Updated

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-﻿
+﻿var basemap;
+//Create the Leaflet map
+function createMap(){
+    //create the map
+    var map = L.map('mapid', {
+        center: [41.257160, -95.995102],
+        zoom:4.5,
+		minzoom:2,
+		maxzoom:18
+    });
+
+    //add OSM base tilelayer
+    basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    }).addTo(map);
+
+    //calling the getData function
+    getData(map);
+};
