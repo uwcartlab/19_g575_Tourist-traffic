45d5b9f8c66063e28a2e382cf36ebad349e89385
AbdullahAmeen <46934578+AbdullahAmeen@users.noreply.github.com>
2019-04-28 12:07:42 -0500

    Tile layer changed and Script added to load the geoJson file

--- js/main.js
+++ js/main.js
@@ -6,15 +6,28 @@ function createMap(){
     //create the map
     var map = L.map('map', {
         center: [32.00903, 118.6969],
-        zoom:10,
+        zoom:12,
 		minzoom:2,
 		maxzoom:18
     });
 
     //add OSM base tilelayer
-    basemap = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
+    var basemap =  L.tileLayer('https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}{r}.png', {
+	attribution: '<a href="https://wikimediafoundation.org/wiki/Maps_Terms_of_Use">Wikimedia</a>',
     }).addTo(map);
 };
-//Begine function create map
+//End function create map
+
+
+//Loading geoJson data file
+
+function addDataToMap(data, map) {
+    var dataLayer = L.geoJson(data);
+    dataLayer.addTo(map);
+}
+
+$.getJSON("data/popular_POIs_AOIs_corrected.geojson", function(data) {addDataToMap(data, map); });
+
+
+
 $(document).ready(createMap);
