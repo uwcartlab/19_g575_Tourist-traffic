748e329df8451606b9f3fbe473b0aa8cc1a79746
AbdullahAmeen <46934578+AbdullahAmeen@users.noreply.github.com>
2019-05-01 17:01:22 -0500

    Search added

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function createMap(){
     //create the map
     var map = L.map('map', {
         center: [32.00983, 118.7969],
-        zoom:12,
+        zoom:11,
 		minzoom:2,
 		maxzoom:18
     });
@@ -21,15 +21,14 @@ function createMap(){
 //End function create map
 
 
-//Begine function to load geoJson data file
+
+//Loading geoJson data file
 function getData (map){
 	$.getJSON("data/popular_POIs_AOIs_corrected.geojson",function(data){
 	
     // Add GeoJSON layer to the map once the file is loaded
-
-		var dataLayer = L.geoJson(data,{
-			
-		// Change Leaflet default markers to circles
+		L.geoJson(data,{
+			// Change Leaflet default markers to circles
 			pointToLayer: function(feature, latlng) {
 				return new L.CircleMarker(latlng, {
 				  radius: 5,
@@ -37,10 +36,12 @@ function getData (map){
 				  weight: 1
 				});
 			  },
-			// Adding popup to POIs
 			onEachFeature:function(feature, layer){
+			
+			// Adding popup to POIs
 				layer.bindPopup(feature.properties.name);
-				//Event listeners to open popup on hover and fill panel on click
+				
+			//Event listeners to open popup on hover and fill panel on click
 				layer.on({
 				mouseover: function(){
 					this.openPopup();
@@ -48,77 +49,10 @@ function getData (map){
 				mouseout: function(){
 					this.closePopup();
 				}
-			}).addTo(map);
+			});
 				console.log(feature.properties)
 			}
-		});
-		
-		//create filter by using leaflet default filter function
-		var overlayMaps = {
-			"PointOfInterest": dataLayer
-		};		
-		L.control.layers(null, overlayMaps).addTo(map);
-		
-		//create search operator
-		createSearchOperator(map, data);
-		
-/* 		//create a L.markerClusterGroup layer
-		var markers = L.markerClusterGroup();
-		//add geojson to marker cluster layer
-		markers.addLayer(dataLayer);
-		//add marker cluster layer to map
-		map.addLayer(markers);
-		markerCluster(map); */
-		
+		}).addTo(map);
 	});
 };
-
-
-//function to create search operator
-function createSearchOperator(map, featCollection){
-	//create search layer
-	var featuresLayer = new L.LayerGroup({
-		style: function(feature) {
-			return {color: feature.properties.color }
-		},
-	});
-
-	map.eachLayer(function(layer){
-		featuresLayer.addLayer(layer);
-	});
-	map.addLayer(featuresLayer);
-
-	//create search control
-	var searchControl = new L.Control.Search({
-		layer: featuresLayer,
-		propertyName: "name",
-		marker: false,
-		zoom: 15,
-	});
-	
-	searchControl.on('search:locationfound', function(e) {
-		e.layer.openPopup();
-	}).on('search_collapsed', function(e) {
-        featuresLayer.resetStyle(layer);
-    });
-	
-	map.addControl( searchControl ); 
-};
-
-//function to create marker cluster
-function markerCluster(map){
-	var markers = L.markerClusterGroup({
-		maxClusterRadius: 120,
-		iconCreatefunction: function(cluster){
-			var markers = cluster.getAllChildMarkers();
-			var n = 0;
-			for (var i = 0; i < markers.length; i++){
-				n += markers[i].number;
-			}
-			return L.divIcon({ html: n, className: 'mycluster', iconSize: L.point(40, 40) });
-		},
-		spiderfyOnMaxZoom: false, showCoverageOnHover: false, zoomToBoundsOnClick: false
-	});
-};
-
 $(document).ready(createMap);
