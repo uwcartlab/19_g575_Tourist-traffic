16d5d3821d85cf46407ed00fb0e188adeeb60f50
AbdullahAmeen <46934578+AbdullahAmeen@users.noreply.github.com>
2019-05-01 06:36:11 -0500

    Retrieve added and styled

--- js/main.js
+++ js/main.js
@@ -21,15 +21,34 @@ function createMap(){
 //End function create map
 
 
-//Loading geoJson data file
+//Begine function to load geoJson data file
 function getData (map){
 	$.getJSON("data/popular_POIs_AOIs_corrected.geojson",function(data){
 	
     // Add GeoJSON layer to the map once the file is loaded
 
 		L.geoJson(data,{
+			
+		// Change Leaflet default markers to circles
+			pointToLayer: function(feature, latlng) {
+				return new L.CircleMarker(latlng, {
+				  radius: 5,
+				  color: "green",
+				  weight: 1
+				});
+			  },
+			// Adding popup to POIs
 			onEachFeature:function(feature, layer){
 				layer.bindPopup(feature.properties.name);
+				//Event listeners to open popup on hover and fill panel on click
+				layer.on({
+				mouseover: function(){
+					this.openPopup();
+				},
+				mouseout: function(){
+					this.closePopup();
+				}
+			});
 				console.log(feature.properties)
 			}
 		}).addTo(map);
